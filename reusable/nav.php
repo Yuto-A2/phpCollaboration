ff10721164506afdcbf536cc6bd47239e32c9801
Yuto Arimori <n01648493@humber.ca>
2024-07-26 12:12:25 -0400

    new file

---
+++ reusable/nav.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="css/style.css">
+
+<div class="headerContainer">
+  <h1><a href=".">Computer Part</a></h1>
+  <nav>
+    <ul>
+      <li><a href=".">Home</a></li>
+      <li><a href="#">Builder</a></li>
+      <li><a href="#">Guides</a></li>
+    </ul>
+  </nav>
+</div> <!-- /headerContainer  -->
